386bfb99cbe8c67cbfd1758a3546e5d4e54c9fe9
marcelhooimeijer <98744131+marcelhooimeijer@users.noreply.github.com>
2022-03-22 13:55:16 +0100

    update 22-03

--- HTML - CSS - JS opdracht versie 5/style.js
+++ HTML - CSS - JS opdracht versie 5/style.js
@@ -32,6 +32,7 @@ menuBtn.addEventListener('click', () => {
 // Formulier validatie
 const naamEl = document.querySelector('#naam');
 const emailadresEl = document.querySelector('#emailadres');
+const telefoonEl = document.querySelector('#telefoon');
 const form = document.querySelector('#feedbackformulier');
 
 const checkNaam = () => {
@@ -50,7 +51,6 @@ const checkNaam = () => {
     }
     return valid;
 };
-
 const checkEmail = () => {
     let valid = false;
     const emailadres = emailadresEl.value.trim();
@@ -64,11 +64,29 @@ const checkEmail = () => {
     }
     return valid;
 };
+const checkTelefoon = () => {
+    let valid = false;
+    const telefoon = telefoonEl.value.trim();
+    if (!isRequired(telefoon)) {
+        showFout(telefoonEl, 'Vul je telefoonnummer in.');
+    } else if (!isTelefoonValid(telefoon)) {
+        showFout(telefoonEl, 'Dit is geen geldig telefoonnummer.');
+    } else {
+        showGoed(telefoonEl);
+        valid = true;
+    }
+    return valid;
+};
 
 const isEmailValid = (emailadres) => {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(emailadres);
 };
+const isTelefoonValid = (telefoon) => {
+    const re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+    return re.test(telefoon);
+};
+
 const isRequired = value => value === '' ? false : true;
 const isBetween = (length, min, max) => length < min || length > max ? false : true;
 
@@ -94,21 +112,20 @@ const showGoed = (input) => {
     fout.textContent = '';
 };
 
-
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
     let isNaamValid = checkNaam(),
-        isEmailValid = checkEmail();
+        isEmailValid = checkEmail(),
+        isTelefoonValid = checkTelefoon();
 
     let isFormValid = isNaamValid &&
-        isEmailValid;
+        isEmailValid && isTelefoonValid;
 
     if (isFormValid) {
 
     }
 });
-
 form.addEventListener('input', function (e) {
     switch (e.target.id) {
         case 'naam':
@@ -117,13 +134,97 @@ form.addEventListener('input', function (e) {
         case 'emailadres':
             checkEmail();
             break;
+        case 'telefoon':
+            checkTelefoon();
+            break;
     }
 });
 
+const limitChar = (element) => {
+    const maxChar = 200;
+    
+    let ele = document.getElementById(element.id);
+    let charLen = ele.value.length;
+    
+    let p = document.getElementById('charCounter');
+    p.innerHTML = maxChar - charLen + ' tekens resterend';
+    
+    if (charLen > maxChar) 
+    {
+        ele.value = ele.value.substring(0, maxChar);
+        p.innerHTML = 0 + ' tekens resterend'; 
+    }
+}
 
+
+
+
+/*
 window.addEventListener("beforeunload", function (e) {
     let confirmationMessage = '';
 
     (e || window.event).returnValue = confirmationMessage; //Gecko + IE
     return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+}); 
+
+const formSubmitting = false;
+const setFormSubmitting = function() { formSubmitting = true; };
+
+window.onload = function() {
+    window.addEventListener("beforeunload", function (e) {
+        if (formSubmitting) {
+            return undefined;
+        }
+
+        var confirmationMessage = 'It looks like you have been editing something. '
+                                + 'If you leave before saving, your changes will be lost.';
+        
+        (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+        return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+    });
+}; */
+
+
+
+
+// Formulier exit validatie
+
+// Variables to store the input text
+let fname = '';
+let lname = '';
+let subject = '';
+
+// Functions to update the input text
+updateFname = () => {
+    fname = document
+        .getElementById('fname').value;
+}
+
+updateLname = () => {
+    lname = document
+        .getElementById('lname').value;
+}
+
+updateSubject = () => {
+    subject = document
+        .getElementById('subject').value;
+}
+
+// Event listener for the 
+// 'beforeunload' event
+window.addEventListener('beforeunload', 
+                        function (e) {
+
+    // Check if any of the input
+    // fields are filled
+    if (fname !== '' ||
+        lname !== '' ||
+        subject !== '') {
+
+        // Cancel the event and
+        // show alert that the unsaved
+        // changes would be lost
+        e.preventDefault();
+        e.returnValue = '';
+    }
 });
